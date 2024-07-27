df1b119d16aa9de0d9efca4018a3cadd6843b174
alexfdp <66030478+alexfdp@users.noreply.github.com>
2024-07-26 21:41:27 -0500

    COMMIT PRUEBA 3

--- index.php
+++ index.php
@@ -31,12 +31,11 @@
 // if (empty($descripcion) && $id_usuario) {
 //     include 'productos/list.php';
 // }
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode('/', $uri);
-if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     //header("HTTP/1.1 404 Not Found");
     echo 'ok todo';
     exit();
 } else {
-    echo 'ok todo';
+    echo 'ok nada';
 }
