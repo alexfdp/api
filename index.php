33e48840c6387f14f49c5a071f6d09f68835718e
alexfdp <66030478+alexfdp@users.noreply.github.com>
2024-07-26 21:01:43 -0500

    commit final de api

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+$data = json_decode(file_get_contents('php://input'), true);
+
+$nombre = isset($data['nombre']) ? $data['nombre'] : '';
+$id_data = isset($data['id']) ? intval($data['id']) : 0;
+$descripcion = isset($data['descripcion']);
+$id_usuario = isset($_GET['id_usuario']) ? intval($_GET['id_usuario']) : 0;
+
+if ($id > 0 && empty($nombre)) {
+    include 'usuarios/read.php';
+}
+
+if ($id == 0 && !empty($nombre) && $id_data == 0 && empty($descripcion)) {
+    include 'usuarios/create.php';
+}
+
+if ($id_data > 0 && empty($nombre)) {
+    include 'usuarios/delete.php';
+}
+
+if ($id_data > 0 && !empty($nombre)) {
+    include 'usuarios/update.php';
+}
+
+if (!empty($descripcion)) {
+    include 'productos/create.php';
+}
+
+if (empty($descripcion) && $id_usuario) {
+    include 'productos/list.php';
+}
--- productos/create.php
+++ productos/create.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 // Conectar a la base de datos
-$conn = new mysqli('localhost', 'root', '', 'api');
+$conn = new mysqli('193.203.168.5', 'u708242902_eliana_154', ']5sP3hj0i#s', 'u708242902_api');
 
 // Verificar la conexión
 if ($conn->connect_error) {
