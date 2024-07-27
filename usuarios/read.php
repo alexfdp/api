1bb50e176c69238260e6f467fb16f5d0b152e5dc
alexfdp <66030478+alexfdp@users.noreply.github.com>
2024-07-26 21:31:08 -0500

    cambios en conexion

--- usuarios/read.php
+++ usuarios/read.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 // Conectar a la base de datos
-$conn = new mysqli('193.203.168.5', 'u708242902_eliana_154', ']5sP3hj0i#s', 'u708242902_api','3306');
+$conn = new mysqli('srv1050.hstgr.io', 'u708242902_eliana_154', ']5sP3hj0i#s', 'u708242902_api');
 
 // Verificar la conexión
 if ($conn->connect_error) {
