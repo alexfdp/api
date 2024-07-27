6c68e4a44a3e376709902e29e8ec5fdfced20ee1
alexfdp <66030478+alexfdp@users.noreply.github.com>
2024-07-26 21:38:41 -0500

    commit prueba 2

--- index.php
+++ index.php
@@ -34,7 +34,8 @@
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
-    header("HTTP/1.1 404 Not Found");
+    //header("HTTP/1.1 404 Not Found");
+    echo 'ok todo';
     exit();
 } else {
     echo 'ok todo';
